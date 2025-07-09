80edfaea62818769cb95a072fae744af4d91bdcb
Ritish <ritish2316@gmail.com>
2025-07-09 23:54:09 +0100

    All Project DONE.

---
+++ app.py
@@ -0,0 +1,133 @@
+import numpy as np
+import streamlit as st
+from tensorflow.keras.models import model_from_json
+from st_audiorec import st_audiorec
+import io
+import librosa
+import scipy
+from skimage.transform import resize
+import tensorflow as tf
+import tensorflow_io as tfio
+import warnings
+
+
+warnings.filterwarnings("ignore")
+
+st.title("Audio Classification using CNNs")
+
+
+@st.cache_resource
+def load_model(model_path_with_model, model_name):
+    with open(f"{model_path_with_model}/{model_name}.json", "r") as json_file:
+        model_json = json_file.read()
+    model = model_from_json(model_json)
+    model.load_weights(f"{model_path_with_model}/{model_name}.weights.h5")
+    return model
+
+
+models_path = "C:\\Users\\ritis\\Desktop\\Audio-Classification-CNNs\\vastai\\working\\models"
+
+waveform_gender = load_model(models_path, "waveform_gender_model")
+waveform_digit = load_model(models_path, "waveform_num_model")
+spectrogram_gender = load_model(models_path, "spectrogram_gender_model")
+spectrogram_digit = load_model(models_path, "spectrogram_num_model")
+
+st.write("Models loaded successfully.")
+
+st.write("Kindly record your voice by clicking on the below button:")
+
+try:
+    wav_audio_data = st_audiorec()
+
+    if wav_audio_data is not None:
+        st.success("Your voice has been successfully recorded.")
+
+        status = st.empty()
+        status.write("Predictions are being made, kindly be patient.")
+
+        audio_file_like_obj = io.BytesIO(wav_audio_data)
+        audio_recording, sampling_rate = librosa.load(
+            audio_file_like_obj, sr=8000, mono=True
+        )
+
+        audio = np.empty((8000,), dtype=np.float32)
+        if len(audio_recording) < 8000:
+            audio = np.pad(audio_recording, (0, 8000 - len(audio_recording)))
+        elif len(audio_recording) > 8000:
+            audio = audio_recording[:8000]
+
+        st.write(
+            "This recording will be an audio input for the models to perform the classification task of speaker's gender and the digit spoken."
+            " This recognition is based on the two ways an audio signal can be represented in."
+        )
+
+        st.write("Waveform based predictions are being made and thus, are as follows:")
+        predicted_gender = np.argmax(
+            waveform_gender.predict(audio.reshape(1, 8000, 1)), axis=1
+        )
+        predicted_digit = np.argmax(
+            waveform_digit.predict(audio.reshape(1, 8000, 1)), axis=1
+        )
+
+        gender = "INF"
+        if predicted_gender == 0:
+            gender = "Male"
+        elif predicted_gender == 1:
+            gender = "Female"
+
+        st.write(f"Gender is {gender}.")
+        st.write(f"Spoken digit is  {predicted_digit[0]}.")
+
+        st.write(
+            "Spectrogram based predictions are being made and thus, are as follows:"
+        )
+
+        f, t, Zxx = scipy.signal.stft(
+            audio, 8000, nperseg=455, noverlap=393, window="hann"
+        )
+
+        Zxx_mag = np.abs(Zxx)
+        spect_final = np.atleast_3d(Zxx_mag)
+
+        spect_mel = tfio.audio.melscale(
+            spect_final, rate=8000, mels=128, fmin=0, fmax=4000
+        )
+
+        spect_log_mel = tf.math.log(spect_mel + 1e-6)
+
+        spect_dB_scaled = tfio.audio.dbscale(spect_log_mel, top_db=80)
+
+        resized = resize(spect_dB_scaled.numpy(), (128, 128, 1), preserve_range=True)
+
+        resized = np.clip(resized, -80, 0)
+        resized = (resized + 80) / 80.0
+
+        resized_spect = tf.reshape(
+            tf.convert_to_tensor(resized, dtype=tf.float32), (1, 128, 128, 1)
+        )
+
+        spect_predicted_gender = np.argmax(
+            spectrogram_gender.predict(resized_spect),
+            axis=1,
+        )
+        spect_predicted_digit = np.argmax(
+            spectrogram_digit.predict(resized_spect),
+            axis=1,
+        )
+
+        sgender = "INF"
+        if spect_predicted_gender == 0:
+            sgender = "Male"
+        elif spect_predicted_gender == 1:
+            sgender = "Female"
+
+        st.write(f"Gender is {sgender}.")
+        st.write(f"Spoken digit is  {spect_predicted_digit[0]}.")
+
+        status.empty()
+
+    # else:
+    #     st.error("Failed to record your audio. Kindly record again. THANKS!")
+
+except Exception as e:
+    print(f"Error occurred: {e}")
---
+++ test.py
@@ -0,0 +1,88 @@
+import numpy as np
+from tensorflow.keras.models import model_from_json
+import librosa
+import scipy
+from skimage.transform import resize
+import tensorflow as tf
+import tensorflow_io as tfio
+import warnings
+import os
+
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+warnings.filterwarnings("ignore")
+
+
+def load_model(model_path_with_model, model_name):
+    with open(f"{model_path_with_model}\\{model_name}.json", "r") as json_file:
+        model_json = json_file.read()
+    model = model_from_json(model_json)
+    model.load_weights(f"{model_path_with_model}\\{model_name}.weights.h5")
+    return model
+
+
+def audio_load(filename):
+    au, sr = librosa.load(filename, sr=8000)
+    audio = np.empty((8000,), dtype=np.float32)
+    if len(au) < 8000:
+        audio = np.pad(au, (0, 8000 - len(au)))
+    elif len(au) > 8000:
+        audio = au[:8000]
+    print("Audio processed.")
+    return audio
+
+
+def spectrogram_generator(audio_in):
+    f, t, zxx = scipy.signal.stft(
+        audio_in, 8000, nperseg=455, noverlap=393, window="hann"
+    )
+    zxx_spect = np.abs(zxx)
+    spectrogram = np.atleast_3d(zxx_spect)
+    mel_spect = tfio.audio.melscale(spectrogram, rate=8000, mels=128, fmin=0, fmax=4000)
+    log_mel_spect = tf.math.log(mel_spect + 1e-6)
+    dbscale_spect = tfio.audio.dbscale(log_mel_spect, top_db=80)
+    resized_spect = resize(dbscale_spect.numpy(), (128, 128, 1), preserve_range=True)
+    resized = tf.convert_to_tensor(resized_spect, dtype=tf.float32)
+    final_spect = tf.reshape(resized, (1, 128, 128, 1))
+    print("Spectrogram generated.")
+    return final_spect
+
+
+def prediction(audio_r, spect, wgmodel, wdmodel, sgmodel, sdmodel):
+    predicted_gender = np.argmax(wgmodel.predict(audio_r.reshape(1, 8000, 1)), axis=1)
+    predicted_digit = np.argmax(wdmodel.predict(audio_r.reshape(1, 8000, 1)), axis=1)
+
+    spect_predicted_gender = np.argmax(sgmodel.predict(spect), axis=1)
+    spect_predicted_digit = np.argmax(sdmodel.predict(spect), axis=1)
+    print("Predictions made.")
+    return (
+        predicted_gender,
+        predicted_digit,
+        spect_predicted_gender,
+        spect_predicted_digit,
+    )
+
+
+if __name__ == "__main__":
+    models_path = (
+        "C:\\Users\\ritis\\Desktop\\Audio-Classification-CNNs\\vastai\\working\\models"
+    )
+
+    waveform_gender = load_model(models_path, "waveform_gender_model")
+    waveform_digit = load_model(models_path, "waveform_num_model")
+    spectrogram_gender = load_model(models_path, "spectrogram_gender_model")
+    spectrogram_digit = load_model(models_path, "spectrogram_num_model")
+
+    audio_mobile = audio_load("Zero-1.wav")
+
+    spect_mobile = spectrogram_generator(audio_mobile)
+
+    wgender, wdigit, sgender, sdigit = prediction(
+        audio_mobile,
+        spect_mobile,
+        waveform_gender,
+        waveform_digit,
+        spectrogram_gender,
+        spectrogram_digit,
+    )
+
+    print(f"Zero: {wgender}, {wdigit}, {sgender}, {sdigit}")
